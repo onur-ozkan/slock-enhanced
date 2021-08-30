142d0335b97538150e5fef572d42c4c55c0e18b7
Onur <onurozkan.dev@outlook.com>
2021-08-30 17:33:39 +0300

    rename config file & update user and group name

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *user  = "root";
+static const char *group = "root";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
